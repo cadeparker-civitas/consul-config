37296c2d2a23fb07d3398b3a9d03e1dd7b27b65f
Cade Parker <cade.parker@civitaslearning.com>
2017-12-12 15:04:32 -0600

    local bootstrapping

---
+++ local/bootstrap-local.sh
@@ -0,0 +1,41 @@
+#!/bin/sh
+
+# get the directory of the script so we know where the config file is located
+BOOTSTRAP_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+BOOTSTRAP_CONFIG=$BOOTSTRAP_DIR/config.json
+
+if ! [ -x "$(command -v consul)" ]; then
+  echo "Could not find installed consul. Running docker image instead..."
+  # if the consul docker process isn't running, then start a new container
+  if [ ! "$(docker ps -q -f ancestor=consul)" ]; then
+    echo "Starting Consul..."
+
+    echo "command: docker run -d -p 8500:8500 consul"
+    docker run -d -p 8500:8500 consul
+
+    # wait for the consul agent to start
+    sleep 5
+  else
+    echo "Consul is already running..."
+  fi
+else
+  if pgrep -x "consul" > /dev/null
+  then
+    echo "Consul is already running..."
+
+  else
+    echo "Starting Consul..."
+
+    echo "command: consul agent -dev"
+    consul agent -dev &
+  fi
+fi
+
+echo "Running git2consul with config:"
+
+cat ${BOOTSTRAP_CONFIG}
+
+echo "\n"
+
+git2consul --config-file ${BOOTSTRAP_CONFIG}
